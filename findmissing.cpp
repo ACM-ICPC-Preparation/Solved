224b665c852e9ead156d7337f001d19c19c314d1
analeemoshtagh <98878012+analeemoshtagh@users.noreply.github.com>
2023-11-09 13:40:43 +0330

    Create findmissing.cpp

---
+++ findmissing.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FOR(A,B,C) for(int A=B ; A<C ; A++)
+
+int main(){
+	vector<int> a{1,3,6,5,2};
+	int size = sizeof(a)/sizeof(a[0]);
+	sort(a.begin(),a.end()); //1,2,3,5,6
+	
+	//Naive method
+	FOR(i,0,*max_element(a.begin(),a.end())){
+		if(find(a.begin(),a.end(),i+1) == a.end()){
+			cout<<i+1<<'\n';
+		}
+	}
+	
+	//Efficient Method
+	cout<<(size*(size+1)/2) - accumulate(a.begin(),a.end(),0);
+	
+	
+	return 0;
+}
